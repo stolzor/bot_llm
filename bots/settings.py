72e060f1df710163eb46a615063884fd8107bd94
stolzor <ruslangataullin16@gmail.com>
2023-09-18 08:59:08 +0300

    update template .env_example and set base class for settings

--- bots/settings.py
+++ bots/settings.py
@@ -1,12 +1,13 @@
 import os
 
+from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
 
-class RedisConfig:
+class RedisConfig(BaseSettings):
     redis_password = os.getenv("REDIS_PASSWORD")
     redis_port = os.getenv("REDIS_PORT")
     connections = os.getenv("REDIS_CONNECTION")
