0d13a750647dfe638b659d9383102fd80bc6afd2
stolzor <ruslangataullin16@gmail.com>
2023-09-18 08:49:08 +0300

    do docker-compose and change abstract class redis

--- bots/middlewares/redis.py
+++ bots/middlewares/redis.py
@@ -1,9 +1,9 @@
-from typing import Any, Callable, Dict, Awaitable
+from abc import ABC
+from typing import Any
 
 from aioredis import BlockingConnectionPool
 from aioredis.client import Redis
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject, Message
 
 from ..settings import RedisConfig
 from logger import get_logger
@@ -12,7 +12,7 @@ from logger import get_logger
 logger = get_logger(__name__, "bot.log")
 
 
-class RedisMiddleware(BaseMiddleware):
+class RedisMiddleware(BaseMiddleware, ABC):
     """Middleware for cache session user."""
 
     def __init__(self, url: str = None, ex: int = 3) -> None:
@@ -29,24 +29,8 @@ class RedisMiddleware(BaseMiddleware):
         )
         self.client = Redis(connection_pool=self.pool)
 
-    async def __call__(
-        self,
-        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-        event: Message,
-        data: Dict[str, Any],
-    ) -> Any:
-        user_id = f"user_{event.from_user.id}"
-
-        check_user = await self.client.get(name=user_id)
-
-        if check_user:
-            if int(check_user.decode()) == 1:
-                await self.client.set(name=user_id, value=0, ex=self.ex)
-                return await event.answer("Wait please I'm not that fast ;)")
-            return
-        await self.client.set(name=user_id, value=1, ex=self.ex)
-
-        return await handler(event, data)
+    async def __call__(self, *args, **kwargs) -> Any:
+        raise NotImplementedError()
 
 
 if __name__ == "__main__":
